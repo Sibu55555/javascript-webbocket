e65229f56c42d4e519cbb847f3cc0b121128cf9d
Sibu55555 <152254576+Sibu55555@users.noreply.github.com>
2023-11-28 07:26:15 +0000

    jajascript basic done

---
+++ 01-basics/test.js
@@ -0,0 +1,9 @@
+console.log("namaste dunia")
+/*
+1. javascript is a high level language used to perform the task on frontend as well as backend.
+2. javascript use both clientside as well as server side.
+3. javascript is most used programming language for web developement.
+4. in javascript web use "Esb" version is the latest one developd in 2015.
+    *Esb=Echma script v-6(vanilla javascript)
+5. javascript is basically used for designing the logic part of our web pages.
+*/
